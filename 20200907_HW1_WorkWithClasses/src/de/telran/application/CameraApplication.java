cdcbc425d6875f9c00123281b3bdfd8b88ed8723
sokhan1705 <sokhan1705@gmail.com>
2020-09-10 21:30:21 +0200

    Add one more camera object

--- 20200907_HW1_WorkWithClasses/src/de/telran/application/CameraApplication.java
+++ 20200907_HW1_WorkWithClasses/src/de/telran/application/CameraApplication.java
@@ -10,6 +10,13 @@ public class CameraApplication {
         cannonD600.setFullFrame(false);
         cannonD600.setModel("cannon d600");
 
+        Camera cannonD6 = new Camera();
+        cannonD6.setModel("cannonD6");
+        cannonD6.setFullFrame(true);
+        cannonD6.setYearOfAssembling(2006);
+        cannonD6.setBrand("cannon");
+
+        System.out.println(cannonD6);
         System.out.println(cannonD600);
     }
 }
